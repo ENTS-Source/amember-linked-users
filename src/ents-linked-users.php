d4d283d3e24e7c0856808407ee98709b546379c9
Travis Ralston <travpc@gmail.com>
2019-01-12 22:01:11 -0700

    Listing of linked users

--- src/ents-linked-users.php
+++ src/ents-linked-users.php
@@ -10,6 +10,12 @@ class Am_Plugin_EntsLinkedUsers extends Am_Plugin
     const PLUGIN_REVISION = "1.0.0";
     const ADMIN_PERM_ID = "ents-link-users";
 
+    protected $_table;
+
+    function getLinkedUsers($userId) {
+        return $this->getDi()->db->select("SELECT * FROM ?_user WHERE user_id in (SELECT user_id_a AS user_id FROM ?_user_links WHERE user_id_b = ?d UNION SELECT user_id_b AS user_id FROM ?_user_links WHERE user_id_a = ?d)", $userId, $userId);
+    }
+
     function onUserTabs(Am_Event_UserTabs $e)
     {
         if ($e->getUserId() > 0) {
@@ -27,9 +33,33 @@ class Am_Plugin_EntsLinkedUsers extends Am_Plugin
         }
     }
 
+    function getTable()
+    {
+        if (!$this->_table) {
+            $this->_table = $this->getDi()->userLinkTable;
+        }
+        return $this->_table;
+    }
+
     function onGetPermissionsList(Am_Event $event)
     {
         $event->addReturn(___("ENTS: Link Users"), self::ADMIN_PERM_ID);
+    }
+
+    static function getDbXml()
+    {
+        return <<<CUT
+<schema version="4.0.0">
+    <table name="user_links">
+        <field name="link_id" type="int" notnull="1" extra="auto_increment"/>
+        <field name="user_id_a" type="int"/>
+        <field name="user_id_b" type="int"/>
+        <index name="PRIMARY" unique="1">
+            <field name="link_id" />
+        </index>
+    </table>
+</schema>
+CUT;
     }
 }
 
@@ -44,8 +74,29 @@ class AdminEntsLinkedUsersController extends Am_Mvc_Controller
 
     public function indexAction()
     {
-        $this->view->content = "<h1>" . ___("Linked Users") . "</h1>";
+        $linkedUsers = $this->getDi()->plugins_misc->loadGet("ents-linked-users")->getLinkedUsers($this->getInt('user_id'));
+        $this->view->content .= "<h1>" . ___("Linked Users") . "</h1>";
+        if (count($linkedUsers) > 0) {
+            $this->view->content .= "<ul>";
+            foreach($linkedUsers as $user) {
+                $this->view->content .= "<li style='margin-left: 20px; list-style: disc'>${user['name_f']} ${user['name_l']} (Fob: ${user['fob']})</li>";
+            }
+            $this->view->content .= "</ul>";
+        } else {
+            $this->view->content .= "<i>No users linked.</i>";
+        }
 
         $this->view->display("admin/user-layout.phtml");
     }
+}
+
+class UserLink extends Am_Record {
+    protected $_key = 'link_id';
+    protected $_table = '?_user_links';
+}
+
+class UserLinkTable extends Am_Table {
+    protected $_key = 'link_id';
+    protected $_table = '?_user_links';
+    protected $_recordClass = 'UserLink';
 }
